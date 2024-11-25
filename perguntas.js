cd8b2ba42d5b12cb7c0d46bad5b0c0159c130b5a
luiz230 <0000107950851xsp@al.educacao.sp.gov.br>
2024-11-25 11:48:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
+
+criaCartao(
+    'Quimica',
+    'Atualmente, quantos elementos químicos a tabela periódica possui',
+    'Atualmente existem cerca de 118 elementos na tabela periodica.'
+)
+
+criaCartao(
+    'Fisica',
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    'A luz do Sol demora cerca de 8 minutos para chegar na Terra.'
+)
+
+criaCartao(
+    'Biologia',
+    'Como se chamam os vasos que transportam sangue do coração para a periferia do corpo?',
+    'São as artérias.'
+)
